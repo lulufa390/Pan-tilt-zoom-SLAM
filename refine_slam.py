6975e261d51986041009bd13e90671f6c6e6faf9
lulufa390 <1042767545@qq.com>
2018-08-06 17:38:57 -0700

    new

--- refine_slam.py
+++ refine_slam.py
@@ -40,7 +40,7 @@ class PtzSlam:
         initialize the fixed parameters of our algorithm
         u, v, base_rotation and c
         """
-        self.u, self.v = self.annotation[0][0]['camera'][0][0:2]
+        self.u, self.v = self.annotation[0][1]['camera'][0][0:2]
         self.base_rotation = np.zeros([3, 3])
         cv.Rodrigues(self.meta[0][0]["base_rotation"][0], self.base_rotation)
         self.c = self.meta[0][0]["cc"][0]
@@ -49,7 +49,7 @@ class PtzSlam:
         parameters to be updated
         """
         self.x = np.ndarray([self.annotation.size, 3])
-        pan, tilt, f = self.annotation[0][0]['ptz'].squeeze()
+        pan, tilt, f = self.annotation[0][1]['ptz'].squeeze()
         self.x[0] = [pan, tilt, f]
 
         self.p = np.ndarray([self.annotation.size, 3, 3])
@@ -57,13 +57,13 @@ class PtzSlam:
 
         self.delta_pan, self.delta_tilt, self.delta_zoom = [0, 0, 0]
 
-        self.q = 0.01* np.diag([1000, 1000, 1])
+        self.q = 0.01 * np.diag([1000, 1000, 1])
         # self.r = 0.01 * np.ones([3, 3])
 
     @staticmethod
     def compute_jacobi(theta, phi, foc, ray):
         jacobi_h = np.ndarray([2, 3])
-        jacobi_h[0][0] = -foc / math.pow(math.cos( math.radians(ray[0] - theta) ), 2)
+        jacobi_h[0][0] = -foc / math.pow(math.cos(math.radians(ray[0] - theta)), 2)
         jacobi_h[0][1] = 0
         jacobi_h[0][2] = math.tan(math.radians(ray[0] - theta))
         jacobi_h[1][0] = 0
@@ -83,7 +83,8 @@ class PtzSlam:
         jacobi_h[1][0] = 0
         jacobi_h[1][1] = (-foc * math.tan(ray[1] - (phi + delta)) + foc * math.tan(ray[1] - (phi - delta))) / (
                 2 * delta)
-        jacobi_h[1][2] = (-(foc + delta) * math.tan(ray[1] - phi) + (foc - delta) * math.tan(ray[1] - phi)) / (2 * delta)
+        jacobi_h[1][2] = (-(foc + delta) * math.tan(ray[1] - phi) + (foc - delta) * math.tan(ray[1] - phi)) / (
+                2 * delta)
         return jacobi_h
 
     @staticmethod
@@ -111,13 +112,22 @@ class PtzSlam:
         features = np.ndarray([len(self.rays), 2])
         for j in range(len(self.rays)):
             pos = np.array(self.pts[j])
-            features[j] = synthesize.from_3d_to_2d(self.u, self.v, f, math.radians(pan), math.radians(tilt), self.c, self.base_rotation, pos)
+            features[j] = synthesize.from_3d_to_2d(self.u, self.v, f, math.radians(pan), math.radians(tilt), self.c,
+                                                   self.base_rotation, pos)
 
         return features
 
     def visualize_features(self, features, pt_color):
         for j in range(len(features)):
             cv.circle(self.img, (int(features[j][0]), int(features[j][1])), color=pt_color, radius=8, thickness=2)
+
+    def get_points_in_image(self, observe, predict, width, height):
+        index_list = []
+        for i in range(len(self.rays)):
+            if 0 < observe[i][0] < width and 0 < observe[i][1] < height and 0 < predict[i][0] < width and 0 < \
+                    predict[i][1] < height:
+                index_list.append(i)
+        return index_list
 
     def extended_kalman_filter(self, previous_x, previous_p, observe):
         predict_x = previous_x + [self.delta_pan, self.delta_tilt, self.delta_zoom]
@@ -127,63 +137,26 @@ class PtzSlam:
         hx = np.ndarray([len(self.rays), 2])
 
         for j in range(len(self.rays)):
-            hx[j][0] = predict_x[2] * math.tan( math.radians(self.rays[j][0] - predict_x[0]) ) + self.u
-            hx[j][1] = - predict_x[2] * math.tan( math.radians(self.rays[j][1] - predict_x[1]) ) + self.v
+            hx[j][0] = predict_x[2] * math.tan(math.radians(self.rays[j][0] - predict_x[0])) + self.u
+            hx[j][1] = - predict_x[2] * math.tan(math.radians(self.rays[j][1] - predict_x[1])) + self.v
 
-        y = []
-        jacobi = []
-        jacobi_a = []
+        index = self.get_points_in_image(observe, hx, 1280, 720)
+        y = np.ndarray([2 * len(index), 1])
+        jacobi = np.ndarray([2 * len(index), 3])
+        for i in range(len(index)):
+            pts_index = index[i]
+            y[2 * i] = observe[pts_index][0] - hx[pts_index][0]
+            y[2 * i + 1] = observe[pts_index][1] - hx[pts_index][1]
 
-        for j in range(len(self.rays)):
-            if 0 < observe[j][0] < 1280 and 0 < observe[j][1] < 720 and 0 < hx[j][0] < 1280 and 0 < hx[j][1] < 720:
-                y.append(observe[j][0] - hx[j][0])
-                y.append(observe[j][1] - hx[j][1])
-                jacobi.append(PtzSlam.compute_jacobi(predict_x[0], predict_x[1], predict_x[2], self.rays[j]))
-                jacobi_a.append(PtzSlam.compute_jacobi_approximate(predict_x[0], predict_x[1], predict_x[2], self.rays[j]))
+            jacobi[2 * i:2 * i + 2, 0:3] = PtzSlam.compute_jacobi(predict_x[0], predict_x[1], predict_x[2],
+                                                                  self.rays[pts_index])
 
-        y = np.array(y)
-        jacobi = np.array(jacobi)
-        jacobi = jacobi.reshape((-1, 3))
-
-        jacobi_a = np.array(jacobi_a)
-        jacobi_a = jacobi_a.reshape((-1, 3))
-
-        # print(jacobi - jacobi_a)
-
-        print(jacobi)
-        # print(predict_p)
-
-        # print(y)
-        # print(jacobi)
-
-        # test = np.array([[1,1,1], [1,2,3], [1,5,1]])
-        # print(np.linalg.inv(test))
-
-
-        # print(np.dot(np.dot(jacobi, predict_p), jacobi.T), "\n\n\n")
-        s = np.dot(np.dot(jacobi, predict_p), jacobi.T) - 1 * np.ones([len(y), len(y)])
-
-        # print(s)
-        # print("inv s", np.linalg.inv(s))
-        # print(y)
-
-
-
+        s = np.dot(np.dot(jacobi, predict_p), jacobi.T) +0.01 * np.ones([2 * len(index), 2 * len(index)])
         k = np.dot(np.dot(predict_p, jacobi.T), np.linalg.inv(s))
-        print(k)
-        # print(k.shape)
-        # print(y.shape)
 
-        # print(np.dot(k,y) * 180 / math.pi)
-        #
-        # fuck = np.dot(k, y)
-        # print(fuck.shape)
-        # print(predict_x.shape)
-
-        # print(predict_x + np.dot(k, y) )
-        # updated_x = np.transpose(predict_x + np.dot(k, y))
-        updated_x = predict_x + np.dot(k, y)
+        updated_x = predict_x + np.dot(k, y).squeeze()
         updated_p = np.dot((np.eye(3) - np.dot(k, jacobi)), predict_p)
+
         return updated_x, updated_p
 
     def main_algorithm(self):
@@ -197,6 +170,8 @@ class PtzSlam:
             self.x[i], self.p[i] = self.extended_kalman_filter(self.x[i - 1], self.p[i - 1], observe)
             self.delta_pan, self.delta_tilt, self.delta_zoom = self.x[i] - self.x[i - 1]
 
+            print(self.x[i])
+
             # estimate_features = self.get_observation_from_ptz(self.x[i][0], self.x[i][1], self.x[i][2])
             # self.visualize_features(observe, (0, 0, 0))
             # self.visualize_features(estimate_features, (0, 0, 255))
@@ -207,23 +182,23 @@ class PtzSlam:
         predict_features = self.get_observation_from_index(0)
         estimate_features = self.get_observation_from_ptz(self.x[1][0], self.x[1][1], self.x[1][2])
 
-
         self.visualize_features(predict_features, (255, 0, 0))
         self.visualize_features(features, (0, 0, 0))
         self.visualize_features(estimate_features, (0, 0, 255))
 
-        print(self.x[1])
-        print(self.annotation[0][1]['ptz'].squeeze())
+        # print(self.x[1])
+        # print(self.annotation[0][1]['ptz'].squeeze())
         cv.imshow("synthesized image", self.img)
         cv.waitKey(0)
 
 
+
+
 # PtzSlam.test_jacobi(0.9, 0.1, 3500, 0.7, 0.5)
-PtzSlam.test_jacobi(0.9, 0.1, 3500, 1.5, 0.8)
-PtzSlam.test_jacobi(0.9, 0.1, 3500, -0.6, 0.5)
+# PtzSlam.test_jacobi(0.9, 0.1, 3500, 1.5, 0.8)
+# PtzSlam.test_jacobi(0.9, 0.1, 3500, -0.6, 0.5)
 
 slam = PtzSlam("./two_point_calib_dataset/util/highlights_soccer_model.mat",
                "./two_point_calib_dataset/highlights/seq3_anno.mat",
                "./synthesize_data.mat")
 slam.main_algorithm()
-
