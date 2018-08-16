d73f0913a0f4a244909036be716c07833f277445
lulufa390 <1042767545@qq.com>
2018-08-15 23:23:08 -0700

    update slam on 8.15 night

--- slam_with_landmark.py
+++ slam_with_landmark.py
@@ -15,7 +15,10 @@ from transformation import TransFunction
 
 class PtzSlam:
     def __init__(self, model_path, annotation_path, data_path):
-        self.img = np.zeros((720, 1280, 3), np.uint8)
+
+        self.width = 1280
+        self.height = 720
+        self.img = np.zeros((self.height, self.width, 3), np.uint8)
 
         """
         load the data of soccer field
@@ -31,8 +34,9 @@ class PtzSlam:
         self.meta = seq['meta']
 
         data = sio.loadmat(data_path)
-        self.all_rays = np.column_stack((data["rays"], data["features"]))
 
+        # this is synthesized rays to generate 2d-point. Real data does not have this variable
+        self.all_rays = np.column_stack((data["rays"], data["features"]))
 
         """
         initialize the fixed parameters of our algorithm
@@ -49,7 +53,6 @@ class PtzSlam:
         self.ray_global = np.ndarray([0, self.all_rays.shape[1]])
         self.p_global = np.zeros([3, 3])
 
-
     def get_ptz(self, index):
         return self.annotation[0][index]['ptz'].squeeze()
 
@@ -60,6 +63,23 @@ class PtzSlam:
         camera_center = self.meta[0][0]["cc"][0]
         return u, v, base_rotation, camera_center
 
+    # given image index, return 2d points and their features
+    def get_image(self, index):
+        pan, tilt, f = self.get_ptz(index)
+        points = np.ndarray([0, self.all_rays.shape[1]])
+
+        for i in range(len(self.all_rays)):
+            x, y = TransFunction.from_pan_tilt_to_2d(self.u, self.v, f, pan, tilt, self.all_rays[i][0],
+                                                     self.all_rays[i][1])
+
+            x += random.randint(-1, 1)
+            y += random.randint(-1, 1)
+
+            if 0 < x < self.width and 0 < y < self.height:
+                points = np.row_stack((points, np.concatenate([np.asarray([x, y]), self.all_rays[i][2:18]], axis=0)))
+
+        return points
+
     def compute_new_jacobi(self, camera_pan, camera_tilt, foc, rays):
         ray_num = len(rays)
 
@@ -69,14 +89,17 @@ class PtzSlam:
         jacobi_h = np.ndarray([2 * ray_num, 3 + 2 * ray_num])
 
         for i in range(ray_num):
-            x_delta_pan1, y_delta_pan1 = TransFunction.from_pan_tilt_to_2d(self.u, self.v, foc, camera_pan - delta_angle,
-                                                                              camera_tilt, rays[i][0], rays[i][1])
-            x_delta_pan2, y_delta_pan2 = TransFunction.from_pan_tilt_to_2d(self.u, self.v, foc, camera_pan + delta_angle, camera_tilt,
-                                                                           rays[i][0], rays[i][1])
+            x_delta_pan1, y_delta_pan1 = TransFunction.from_pan_tilt_to_2d(
+                self.u, self.v, foc, camera_pan - delta_angle, camera_tilt, rays[i][0], rays[i][1])
 
-            x_delta_tilt1, y_delta_tilt1 = TransFunction.from_pan_tilt_to_2d(self.u, self.v, foc, camera_pan, camera_tilt - delta_angle,
+            x_delta_pan2, y_delta_pan2 = TransFunction.from_pan_tilt_to_2d(
+                self.u, self.v, foc, camera_pan + delta_angle, camera_tilt, rays[i][0], rays[i][1])
+
+            x_delta_tilt1, y_delta_tilt1 = TransFunction.from_pan_tilt_to_2d(self.u, self.v, foc, camera_pan,
+                                                                             camera_tilt - delta_angle,
                                                                              rays[i][0], rays[i][1])
-            x_delta_tilt2, y_delta_tilt2 = TransFunction.from_pan_tilt_to_2d(self.u, self.v, foc, camera_pan, camera_tilt + delta_angle,
+            x_delta_tilt2, y_delta_tilt2 = TransFunction.from_pan_tilt_to_2d(self.u, self.v, foc, camera_pan,
+                                                                             camera_tilt + delta_angle,
                                                                              rays[i][0], rays[i][1])
 
             x_delta_f1, y_delta_f1 = TransFunction.from_pan_tilt_to_2d(self.u, self.v, foc - delta_f, camera_pan,
@@ -85,158 +108,169 @@ class PtzSlam:
                                                                        , camera_tilt, rays[i][0], rays[i][1])
 
             x_delta_theta1, y_delta_theta1 = TransFunction.from_pan_tilt_to_2d(self.u, self.v, foc, camera_pan,
-                                                                       camera_tilt, rays[i][0] - delta_angle, rays[i][1])
+                                                                               camera_tilt, rays[i][0] - delta_angle,
+                                                                               rays[i][1])
             x_delta_theta2, y_delta_theta2 = TransFunction.from_pan_tilt_to_2d(self.u, self.v, foc, camera_pan
-                                                                       , camera_tilt, rays[i][0] + delta_angle, rays[i][1])
+                                                                               , camera_tilt, rays[i][0] + delta_angle,
+                                                                               rays[i][1])
 
             x_delta_phi1, y_delta_phi1 = TransFunction.from_pan_tilt_to_2d(self.u, self.v, foc, camera_pan,
-                                                                       camera_tilt, rays[i][0], rays[i][1] - delta_angle)
+                                                                           camera_tilt, rays[i][0],
+                                                                           rays[i][1] - delta_angle)
             x_delta_phi2, y_delta_phi2 = TransFunction.from_pan_tilt_to_2d(self.u, self.v, foc, camera_pan
-                                                                       , camera_tilt, rays[i][0] , rays[i][1] + delta_angle)
+                                                                           , camera_tilt, rays[i][0],
+                                                                           rays[i][1] + delta_angle)
 
-            jacobi_h[2*i][0] = (x_delta_pan2 - x_delta_pan1) / (2 * delta_angle)
-            jacobi_h[2*i][1] = (x_delta_tilt2 - x_delta_tilt1) / (2 * delta_angle)
-            jacobi_h[2*i][2] = (x_delta_f2 - x_delta_f1) / (2 * delta_f)
+            jacobi_h[2 * i][0] = (x_delta_pan2 - x_delta_pan1) / (2 * delta_angle)
+            jacobi_h[2 * i][1] = (x_delta_tilt2 - x_delta_tilt1) / (2 * delta_angle)
+            jacobi_h[2 * i][2] = (x_delta_f2 - x_delta_f1) / (2 * delta_f)
 
-            jacobi_h[2*i+1][0] = (y_delta_pan2 - y_delta_pan1) / (2 * delta_angle)
-            jacobi_h[2*i+1][1] = (y_delta_tilt2 - y_delta_tilt1) / (2 * delta_angle)
-            jacobi_h[2*i+1][2] = (y_delta_f2 - y_delta_f1) / (2 * delta_f)
+            jacobi_h[2 * i + 1][0] = (y_delta_pan2 - y_delta_pan1) / (2 * delta_angle)
+            jacobi_h[2 * i + 1][1] = (y_delta_tilt2 - y_delta_tilt1) / (2 * delta_angle)
+            jacobi_h[2 * i + 1][2] = (y_delta_f2 - y_delta_f1) / (2 * delta_f)
 
             for j in range(ray_num):
                 if j == i:
-                    jacobi_h[2 * i][3 + 2*j] = (x_delta_theta2 - x_delta_theta1) / (2 * delta_angle)
-                    jacobi_h[2 * i][3 + 2*j +1] = (x_delta_phi2 - x_delta_phi1) / (2 * delta_angle)
+                    jacobi_h[2 * i][3 + 2 * j] = (x_delta_theta2 - x_delta_theta1) / (2 * delta_angle)
+                    jacobi_h[2 * i][3 + 2 * j + 1] = (x_delta_phi2 - x_delta_phi1) / (2 * delta_angle)
 
-                    jacobi_h[2 * i+1][3 + 2 * j] = (y_delta_theta2 - y_delta_theta1) / (2 * delta_angle)
-                    jacobi_h[2 * i+1][3 + 2 * j + 1] = (y_delta_phi2 - y_delta_phi1) / (2 * delta_angle)
+                    jacobi_h[2 * i + 1][3 + 2 * j] = (y_delta_theta2 - y_delta_theta1) / (2 * delta_angle)
+                    jacobi_h[2 * i + 1][3 + 2 * j + 1] = (y_delta_phi2 - y_delta_phi1) / (2 * delta_angle)
                 else:
                     jacobi_h[2 * i][3 + 2 * j] = jacobi_h[2 * i][3 + 2 * j + 1] = \
-                    jacobi_h[2 * i + 1][3 + 2 * j] = jacobi_h[2 * i + 1][3 + 2 * j + 1] = 0
+                        jacobi_h[2 * i + 1][3 + 2 * j] = jacobi_h[2 * i + 1][3 + 2 * j + 1] = 0
 
         return jacobi_h
 
-    def get_observation_from_ptz(self, pan, tilt, f, rays, width, height):
-        points = np.ndarray([0, 2])
+    # return all 2d points, corresponding rays and indexes of these points IN THE IMAGE.
+    def get_observation_from_rays(self, pan, tilt, f, rays):
+        points = np.ndarray([0, rays.shape[1]])
         inner_rays = np.ndarray([0, rays.shape[1]])
         index = np.ndarray([0])
 
         for j in range(len(rays)):
             tmp = TransFunction.from_pan_tilt_to_2d(self.u, self.v, f, pan, tilt, rays[j][0], rays[j][1])
-            if 0 < tmp[0] < width and 0 < tmp[1] < height:
+            if 0 < tmp[0] < self.width and 0 < tmp[1] < self.height:
                 inner_rays = np.row_stack((inner_rays, rays[j]))
-                points = np.row_stack((points, tmp))
+                points = np.row_stack((points, np.concatenate([np.asarray(tmp), rays[j][2:18]], axis=0)))
                 index = np.concatenate((index, [j]), axis=0)
         return points, inner_rays, index
 
+    # get a list of rays from 2d points and camera pose
+    def get_rays_from_observation(self, pan, tilt, f, points):
+        rays = np.ndarray([0, points.shape[1]])
+        for i in range(len(points)):
+            angles = TransFunction.from_2d_to_pan_tilt(self.u, self.v, f, pan, tilt, points[i][0], points[i][1])
+            rays = np.row_stack((rays, np.concatenate([angles, points[i][2:18]], axis=0)))
+        return rays
 
+    # draw some colored points in img
     def visualize_points(self, points, pt_color):
         for j in range(len(points)):
             cv.circle(self.img, (int(points[j][0]), int(points[j][1])), color=pt_color, radius=8, thickness=2)
 
-
     def main_algorithm(self):
 
-        self.img.fill(255)
+        random.seed(1)
 
         self.camera_pose = self.get_ptz(0)
-        points, in_rays, index = self.get_observation_from_ptz(self.camera_pose[0], self.camera_pose[1],
-                                                   self.camera_pose[2],self.all_rays, 1280, 720)
+        first_frame = self.get_image(0)
+
+        init_rays = self.get_rays_from_observation(self.camera_pose[0], self.camera_pose[1], self.camera_pose[2],
+                                                   first_frame)
+
 
         # add rays in frame 1 to global rays
-        self.ray_global = np.concatenate([self.ray_global, in_rays], axis=0)
+        self.ray_global = np.concatenate([self.ray_global, init_rays], axis=0)
 
         # initialize global p using global rays
-        self.p_global = 0.01 * np.eye(3+2*len(self.ray_global))
+        self.p_global = 0.01 * np.eye(3 + 2 * len(self.ray_global))
         self.p_global[2][2] = 1
 
         q_k = 1 * np.diag([0.01, 0.01, 1])
 
-        # print(self.p_global)
+        for i in range(1, 10):
+            self.img.fill(255)
 
-
-        for i in range(1, 2):
             # ground truth for next frame. In real data do not need to compute
-            next_pan, next_tilt, next_f = self.get_ptz(i)
-            points_next, in_rays_next, _ = self.get_observation_from_ptz(next_pan, next_tilt, next_f, self.all_rays, 1280, 720)
 
+            next_frame = self.get_image(i)
+
+            # next_pan, next_tilt, next_f = self.get_ptz(i)
+            # points_next, in_rays_next, index_in_all = self.get_observation_from_rays(next_pan, next_tilt, next_f,
+            #                                                                         self.all_rays)
 
             self.camera_pose += [self.delta_pan, self.delta_tilt, self.delta_zoom]
-            points_before, in_rays_before, index_before = self.get_observation_from_ptz(self.camera_pose[0], self.camera_pose[1],
-                                                                          self.camera_pose[2], self.ray_global, 1280, 720)
 
-            print(self.camera_pose)
+            predict_points, predict_rays, inner_point_index = self.get_observation_from_rays(
+                self.camera_pose[0], self.camera_pose[1], self.camera_pose[2], self.ray_global)
 
             self.p_global[0:3, 0:3] = self.p_global[0:3, 0:3] + q_k
 
             y_k = np.ndarray([0])
-            for j in range(len(in_rays_before)):
+            for j in range(len(predict_points)):
                 flag = False
-                for k in range(len(in_rays_next)):
-                    if np.linalg.norm(in_rays_next[k][2:18] - in_rays_before[j][2:18]) < 0.01:
-
-                        y_k = np.concatenate((y_k, points_next[k] - points_before[j]), axis=0)
+                for k in range(len(next_frame)):
+                    if np.linalg.norm(next_frame[k][2:18] - predict_points[j][2:18]) < 0.01:
+                        y_k = np.concatenate((y_k, next_frame[k][0:2] - predict_points[j][0:2]), axis=0)
                         flag = True
                 if not flag:
-                    index_before = np.delete(index_before, j, axis=0)
-
+                    inner_point_index = np.delete(inner_point_index, j, axis=0)
 
             predict_x = self.camera_pose
-            for j in range(len(index_before)):
-                predict_x = np.concatenate([predict_x, self.ray_global[int(index_before[j])][0:2]], axis=0)
+            for j in range(len(inner_point_index)):
+                predict_x = np.concatenate([predict_x, self.ray_global[int(inner_point_index[j])][0:2]], axis=0)
 
-            p_index = (np.concatenate([[0,1,2], index_before+3, index_before+len(index_before) + 3])).astype(int)
-            # print("fuck", index_before)
-            p_tmp = self.p_global[p_index]
-            # print(p_tmp)
-            p = p_tmp[:, p_index]
+            # get p matrix for this iteration from p_global
+            p_index = (np.concatenate([[0, 1, 2], inner_point_index + 3, inner_point_index + len(inner_point_index) + 3])).astype(int)
 
-            # print(p)
+            p = self.p_global[p_index][:, p_index]
 
-            # print(predict_x)
             # compute jacobi
-            jacobi = self.compute_new_jacobi(camera_pan=self.camera_pose[0],  camera_tilt=self.camera_pose[1],
-                                             foc=self.camera_pose[2], rays=self.ray_global[index_before.astype(int)])
+            jacobi = self.compute_new_jacobi(camera_pan=self.camera_pose[0], camera_tilt=self.camera_pose[1],
+                                             foc=self.camera_pose[2], rays=self.ray_global[inner_point_index.astype(int)])
 
-            # print(self.ray_global[index_before.astype(int)])
-
-            # print(jacobi.shape)
-
-            s_k = np.dot(np.dot(jacobi, p), jacobi.T) + np.eye(2*len(index_before))
+            s_k = np.dot(np.dot(jacobi, p), jacobi.T) + np.eye(2 * len(inner_point_index))
 
             k_k = np.dot(np.dot(p, jacobi.T), np.linalg.inv(s_k))
 
-            # print(k_k.shape)
-            # print(y_k.shape)
             k_mul_y = np.dot(k_k, y_k)
-
-            # print(k_mul_y)
 
             self.camera_pose += k_mul_y[0:3]
 
-            print(self.ray_global[index_before.astype(int)])
+            # update speed model
+            self.delta_pan, self.delta_tilt, self.delta_zoom = k_mul_y[0:3]
 
-            print(self.camera_pose)
+            # tmp_rays = self.get_rays_from_observation(self.camera_pose[0], self.camera_pose[1], self.camera_pose[2],
+            #                                           points_next)
+            # print("Before update: ",
+            #       tmp_rays[:,0:2] - self.all_rays[index_in_all.astype(int)][:, 0:2])
 
-            for i in range(len(index_before)):
-                self.ray_global[int(index_before[i])][0:2] += k_mul_y[2*i +3: 2*i+5]
+            for j in range(len(inner_point_index)):
+                self.ray_global[int(inner_point_index[j])][0:2] += k_mul_y[2 * j + 3: 2 * j + 5]
 
-            print(self.ray_global[index_before.astype(int)])
+            # print("After update: ",
+            #       self.ray_global[inner_point_index.astype(int)][:, 0:2] - self.all_rays[index_in_all.astype(int)][:, 0:2])
 
-            points_update, in_rays_update, index_update = self.get_observation_from_ptz(self.camera_pose[0],
-                                                                                       self.camera_pose[1],
-                                                                                       self.camera_pose[2],
-                                                                                       self.ray_global, 1280, 720)
-
-        self.visualize_points(points_before, (255, 0, 0))
-        self.visualize_points(points_next, (0,0,0))
-        self.visualize_points(points_update, (0, 0, 255))
+            update_p = np.dot(np.eye(3 + 2 * len(inner_point_index)) - np.dot(k_k, jacobi), p)
+            for j in range(len(inner_point_index)):
+                for k in range(len(inner_point_index)):
+                    self.p_global[int(inner_point_index[j]), int(inner_point_index[k])] = update_p[j,k]
 
 
-        print("update\n", points_update)
-        print("next\n", points_next)
+            points_update, in_rays_update, index_update = self.get_observation_from_rays(self.camera_pose[0],
+                                                                                        self.camera_pose[1],
+                                                                                        self.camera_pose[2],
+                                                                                        self.ray_global)
 
-        cv.imshow("test", self.img)
-        cv.waitKey(0)
+            self.visualize_points(predict_points, (255, 0, 0))
+            self.visualize_points(next_frame, (0, 0, 0))
+            self.visualize_points(points_update, (0, 0, 255))
+
+            cv.imshow("test", self.img)
+            cv.waitKey(0)
+
+
 
 
 slam = PtzSlam("./two_point_calib_dataset/util/highlights_soccer_model.mat",
