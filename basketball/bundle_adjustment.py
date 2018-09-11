fb7efc0456e497b311dea98762a78e5939629bda
jimmy <jimmy@westlake.cs.ubc.ca>
2018-09-10 20:11:45 -0700

    update bundle adjustment

--- basketball/bundle_adjustment.py
+++ basketball/bundle_adjustment.py
@@ -9,7 +9,18 @@ import math
 from key_frame import KeyFrame
 from image_process import build_matching_graph
 from sequence_manager import SequenceManager
+from transformation import TransFunction
 
+
+# internal class
+class _FrameToFrameMatch:
+    def __init__(self, src_index, des_index, src_pts_index, des_pts_index, landmark_index):
+        self.src_index = src_index
+        self.des_index = des_index
+
+        self.src_pts_index = src_pts_index
+        self.des_pts_index = des_pts_index
+        self.landmark_index = landmark_index
 
 def bundle_adjustment(keyframes):
     """
@@ -17,7 +28,76 @@ def bundle_adjustment(keyframes):
     :param keyframes:
     :return:  optimized keyframes
     """
-    pass
+    def compute_residual(x, n_pose, n_landmark, n_residual, keypoints, src_pt_index, dst_pt_index, landmark_index, u, v):
+        """
+        :param x: N * 3 camera pose, pan, tilt, focal_length + M * 2 landmark, (pan, tilt)
+        :n_pose: camera pose number
+        :n_landmark: landmark number
+        :n_residual: number of residuals
+        :param keypoints: list of N*2 matrix, (x, y) in each frame
+        : src_pt_index, dst_pt_index, landmark_index: 2D list of indices
+        :u, v: image center
+        :return: residual
+        """
+        N = len(keypoints)
+        assert n_pose == N
+        assert len(src_pt_index) == N
+        assert len(dst_pt_index) == N
+        assert len(landmark_index) == N
+
+        for i in range(N):
+            assert len(src_pt_index[i]) == N
+            assert len(dst_pt_index[i]) == N
+            assert len(landmark_index[i]) == N
+
+        landmark_start_index = n_pose * 3
+
+        residual = np.ndarray(n_residual)
+        residual_idx = 0
+        for i in range(N):
+            for j in range(N):
+                pan1, tilt1, fl1 = x[i * 3 + 0], x[i * 3 + 1], x[i * 3 + 2]  # camera pose
+                pan2, tilt2, fl2 = x[j * 3 + 0], x[j * 3 + 1], x[j * 3 + 2]
+
+                # keypoint index
+                for idx1, idx2, idx3 in zip(src_pt_index[i][j], dst_pt_index[i][j], landmark_index[i][j]):
+                    pan, tilt = x[landmark_start_index + idx3 * 2], x[landmark_start_index + idx3 * 2 + 1]  # landmark pan, tilt
+                    x1, y1 = keypoints[i][idx1][0], keypoints[i][idx2][1]
+                    x2, y2 = keypoints[j][idx2][0], keypoints[j][idx2][1]
+                    proj_x1, proj_y1 = TransFunction.from_pan_tilt_to_2d(u, v, fl1, pan1, tilt1, pan, tilt)
+                    proj_x2, proj_y2 = TransFunction.from_pan_tilt_to_2d(u, v, fl2, pan2, tilt2, pan, tilt)
+
+                    # point in camera i
+
+                    if i == 0:
+                        residual[residual_idx] = 0
+                    else:
+                        residual[residual_idx] = proj_x1 - x1
+                    residual_idx += 1
+
+                    if i == 0:
+                        residual[residual_idx] = 0
+                    else:
+                        residual[residual_idx] = proj_y1 - y1
+                    residual_idx += 1
+
+                    # point in camera j
+                    if j == 0:
+                        residual[residual_idx] = 0
+                    else:
+                        residual[residual_idx] = proj_x2 - x2
+                    residual_idx += 1
+
+                    if j == 0:
+                        residual[residual_idx] = 0
+                    else:
+                        residual[residual_idx] = proj_y2 - y2
+                    residual_idx += 1
+
+        assert residual_idx == n_residual
+        return residual
+
+
 
 
 
@@ -65,8 +145,8 @@ def ut_test_build_adjustment():
         key_frames.append(key_frame)
         #print(ptz)
 
+    # step 1: rough matching pairs
     images = [key_frames[i].img for i in range(len(key_frames))]
-
     image_match_mask = [[0 for i in range(N)] for i in range(N)]
     overlap_angle_threshold = 5
     for i in range(N):
@@ -78,9 +158,36 @@ def ut_test_build_adjustment():
             if angle > overlap_angle_threshold:
                 image_match_mask[i][j] = 1
 
-    #print(image_match_mask)
-    keypoints, descriptors, landmark_index = build_matching_graph(images, image_match_mask, True)
+    # step 2: keypoint matching in image pairs
+    # matching keypoints in images
+    keypoints, descriptors, src_pt_index, dst_pt_index, landmark_index, n_landmark = build_matching_graph(images, image_match_mask, True)
+
+
+    # prepare data from least square optimization
+    # def compute_residual(x, n_pose, n_landmark, n_residual, keypoints, src_pt_index, dst_pt_index, landmark_index, u, v):
+    n_residual = 0
+    for i in range(N):
+        for j in range(N):
+            n_residual += len(src_pt_index[i][j]) * 2
+    print('residual number is %d.' % n_residual)
+
+    # initial value of pose and rays
+
+
+
+
+def ut_least_square():
+
+    def fun_rosenbrock(x):
+        return np.array([10 * (x[1] - x[0]**2) , (1 - x[0])])
+
+    from scipy.optimize import least_squares
+    x0 = np.array([2, 2])
+    res = least_squares(fun_rosenbrock, x0)
+    print(res.x)
 
 
 if __name__ == '__main__':
     ut_test_build_adjustment()
+
+    #ut_least_square()
