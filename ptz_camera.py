9d93cc2fb874865fcb4bcbd0b3e7ed337c9a70bc
lulufa390 <1042767545@qq.com>
2018-09-28 20:47:00 -0700

    add comments to ptz_camera

--- ptz_camera.py
+++ ptz_camera.py
@@ -1,22 +1,47 @@
+"""
+PTZCamera class.
+
+Created by Luke, 2018.9
+"""
+
 import numpy as np
 from math import *
 
 
 class PTZCamera:
+    """
+    This is a class for pan-tilt-zoom camera.
+    It provides a bunch of functions for projection and reprojection given camera pose.
+    """
     def __init__(self, principal_point, camera_center, base_rotation):
+        """
+        :param principal_point: principal point (u, v).
+        :param camera_center: camera projection center.
+        :param base_rotation: base rotation matrix [3, 3] array.
+        """
         self.principal_point = principal_point
         self.camera_center = camera_center
         self.base_rotation = base_rotation
 
+        # set pan, tilt, focal length to default value
+        # pan, tilt here are in degree
         self.pan = 0.0
         self.tilt = 0.0
         self.focal_length = 2000
 
     def set_ptz(self, ptz):
+        """
+        Set pan, tilt, focal length for camera.
+        :param ptz: array, tuple, or list [3] of pan(in degree), tilt(in degree), focal_length.
+        """
         self.pan, self.tilt, self.focal_length = ptz
 
     def project_3d_point(self, p):
-        # p is a 3D point
+        """
+        Project a 3d point in world coordinate to image.
+        :param p: 3d point of array [3]
+        :return: projected image point tuple(2)
+        """
         pan = radians(self.pan)
         tilt = radians(self.tilt)
 
@@ -39,6 +64,13 @@ class PTZCamera:
         return position[0] / position[2], position[1] / position[2]
 
     def project_3d_points(self, ps, height=0, width=0):
+        """
+        Project a array of 3d points to image.
+        :param ps: [n, 3] array of n 3d points in world coordinate.
+        :param height: height of image.
+        :param width: width of image.
+        :return: projected points in image range ([m, 2] array) and its index in ps
+        """
         points = np.ndarray([0, 2])
         index = np.ndarray([0])
 
@@ -56,6 +88,11 @@ class PTZCamera:
         return points, index
 
     def project_ray(self, ray):
+        """
+        Project a ray in tripod coordinate to image.
+        :param ray: ray is a array, tuple or list of [2]
+        :return: projected image point tuple(2)
+        """
         theta = radians(ray[0])
         phi = radians(ray[1])
         pan = radians(self.pan)
@@ -80,7 +117,14 @@ class PTZCamera:
         return position[0] / position[2], position[1] / position[2]
 
     def project_rays(self, rays, height=0, width=0):
-        points = np.ndarray([0, 2])
+        """
+        Project a array of rays to image.
+        :param ps: [n, 2] array of n rays in tripod coordinates.
+        :param height: height of image.
+        :param width: width of image.
+        :return: projected points in image range ([m, 2] array) and its index in ps
+        """
+        points = np.ndarray([0, 2], np.float32)
         index = np.ndarray([0])
 
         if height != 0 and width != 0:
@@ -97,7 +141,18 @@ class PTZCamera:
         return points, index
 
     def back_project_to_3d_point(self, x, y):
+        """
+        Back project image point to 3d point.
+        The 3d points on the same ray are all corresponding to the image point.
+        So you should set a dimension (z) to determine that 3d point.
+        :param x: image point location x
+        :param y: image point location y
+        :return: array [3] of image point
+        """
+
+        # set z(3d point) here.
         z = 0
+
         pan = radians(self.pan)
         tilt = radians(self.tilt)
 
@@ -123,7 +178,25 @@ class PTZCamera:
 
         return p
 
+    def back_project_to_3d_points(self, keypoints):
+        """
+        Back project a bunch of image points to 3d points.
+        :param keypoints: [n, 2] array of a array of points on image.
+        :return: [n, 3] array of corresponding 3d point.
+        """
+        points_3d = np.ndarray([0, 3])
+        for i in range(len(keypoints)):
+            point_3d = self.back_project_to_3d_point(keypoints[i, 0], keypoints[i, 1])
+            points_3d = np.row_stack([points_3d, point_3d])
+        return points_3d
+
     def back_project_to_ray(self, x, y):
+        """
+        Back project image point to ray.
+        :param x: image point x.
+        :param y: image point y.
+        :return: tuple (2) of ray: pan, tilt in degree.
+        """
         pan = radians(self.pan)
         tilt = radians(self.tilt)
 
@@ -140,9 +213,6 @@ class PTZCamera:
                                     [sin(pan), 0, cos(pan)]]))
 
         inv_mat = np.linalg.inv(np.dot(k, rotation))
-
-        # rotation = np.linalg.inv(rotation)
-
         x3d, y3d, z3d = np.dot(inv_mat, np.array([x, y, 1]))
 
         theta = atan(x3d / z3d)
@@ -151,6 +221,12 @@ class PTZCamera:
         return degrees(theta), degrees(phi)
 
     def back_project_to_rays(self, points):
+        """
+        Back project a bunch of image points to rays.
+        :param points: [n, 2] array of image points.
+        :return: [n, 2] array of corresponding rays.
+        """
+
         rays = np.ndarray([0, 2])
         for i in range(len(points)):
             angles = self.back_project_to_ray(points[i, 0], points[i, 1])
@@ -159,6 +235,5 @@ class PTZCamera:
 
 
 if __name__ == '__main__':
-    # add some unit test in here
-    # especially for two back projection function
+
     pass
