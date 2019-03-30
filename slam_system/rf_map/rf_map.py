266109a91026b6b7078d26bfe1a817249b2d76e5
jimmy <jimmy@Jianhuis-Mac-mini.local>
2019-03-30 07:34:53 -0700

    add RF map interface

---
+++ slam_system/rf_map/rf_map.py
@@ -0,0 +1,17 @@
+# random forest as map
+
+class RFMap:
+    """
+    The map is saved on files.
+    """
+    def __init__(self, rf_file_name):
+        self.rf_file_name = rf_file_name
+
+    def createMap(self):
+        pass
+
+    def updateMap(self):
+        pass
+
+    def relocalization(self):
+        pass
