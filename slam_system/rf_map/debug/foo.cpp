8891351057347de65609ef8054a16bdf025f3897
jimmy <jianhui.chenn@gmail.com>
2019-07-04 18:27:31 -0700

    add foo debug

---
+++ slam_system/rf_map/debug/foo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Foo{
+    public:
+        void bar(){
+            std::cout << "Hello" << std::endl;
+        }
+};
+
+extern "C" {
+    Foo* Foo_new(){ return new Foo(); }
+    void Foo_bar(Foo* foo){ foo->bar(); }
+}
+
+
---
+++ slam_system/rf_map/debug/foo_wrapper.py
@@ -0,0 +1,12 @@
+from ctypes import cdll
+lib = cdll.LoadLibrary('./libfoo.so')
+
+class Foo(object):
+    def __init__(self):
+        self.obj = lib.Foo_new()
+
+    def bar(self):
+        lib.Foo_bar(self.obj)
+
+f = Foo()
+f.bar()
