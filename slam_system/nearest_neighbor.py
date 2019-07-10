a8012017e1b557c5d20a77469613fde9972238f8
lulufa390 <1042767545@qq.com>
2019-07-10 14:54:28 +0800

    test for data

--- slam_system/nearest_neighbor.py
+++ slam_system/nearest_neighbor.py
@@ -6,11 +6,14 @@ A nearest neighbor search based relocalization solution.
 
 import numpy as np
 import pyflann
+import scipy.io as sio
+import cv2 as cv
 
 from scene_map import Map
 from ptz_camera import PTZCamera
 from scipy.optimize import least_squares
 from transformation import TransFunction
+
 
 
 class NNBasedMap(Map):
@@ -34,7 +37,7 @@ class NNBasedMap(Map):
         matched_keypoint_index = []
         matched_ray_index = []
         for i in range(len(result)):
-            if dist[i] < 1000:
+            if dist[i] < 2000:
                 matched_keypoint_index.append(i)
                 matched_ray_index.append(result[i])
 
@@ -58,7 +61,8 @@ class NNBasedMap(Map):
 
         self.build_kdtree()
 
-    def compute_residual(self, pose, rays, points, u, v):
+    @staticmethod
+    def compute_residual(pose, rays, points, u, v):
         """
         :param pose: shape [3] array of camera pose
         :param rays: [N, 2] array of corresponding rays
@@ -90,10 +94,53 @@ class NNBasedMap(Map):
 
         # fuck = self.compute_residual(pose, rays, keyframe.feature_pts, keyframe.u, keyframe.v)
 
-        optimized_pose = least_squares(self.compute_residual, pose, verbose=2, x_scale='jac', ftol=1e-4, method='trf',
+        optimized_pose = least_squares(NNBasedMap.compute_residual, pose, verbose=2, x_scale='jac', ftol=1e-4, method='trf',
                                        args=(rays, keyframe.feature_pts[keypoint_index], keyframe.u, keyframe.v))
 
+
+        # save to mat
+        keyframe_data = dict()
+
+        keyframe_data['rays'] = rays
+        keyframe_data['keypoints'] = keyframe.feature_pts[keypoint_index]
+
+        # convert the base rotation to (3, 1)
+        save_br = np.ndarray([3, 1])
+        if keyframe.base_rotation.shape == (3, 3):
+            save_br, _ = cv.Rodrigues(keyframe.base_rotation, save_br)
+        else:
+            save_br = keyframe.base_rotation
+        save_br = save_br.ravel()
+
+        keyframe_data['camera'] = np.array([keyframe.u, keyframe.v, keyframe.f, save_br[0], save_br[1], save_br[2],
+                                            keyframe.center[0], keyframe.center[1], keyframe.center[2]]).reshape(-1, 1)
+
+        keyframe_data['ptz'] = np.array([keyframe.pan, keyframe.tilt, keyframe.f]).reshape(-1, 1)
+
+        sio.savemat("../nn_test_data/outliers-0/" + str(keyframe.img_index) + ".mat", mdict=keyframe_data)
+
         return optimized_pose.x
+
+
+def ut_estimateCameraRANSAC():
+    import scipy.io as sio
+
+    for i in range(0, 3600, 120):
+        file_name = 'C:/graduate_design/Pan-tilt-zoom-SLAM/nn_test_data/outliers-0/{}.mat'.format(i)
+        data = sio.loadmat(file_name)
+
+        ptz_gt = data['ptz']
+        init_ptz = np.array([0, 0, 3000])
+
+        rays = data['rays']
+        keypoints = data['keypoints']
+
+        optimized_pose = least_squares(NNBasedMap.compute_residual, init_ptz, verbose=2, x_scale='jac', ftol=1e-4, method='trf',
+                                       args=(rays, keypoints, 640, 360))
+
+
+        print('estimated ptz is {}'.format(optimized_pose.x))
+        print('ground truth ptz is {}'.format(ptz_gt))
 
 
 def ut_pyflann():
@@ -112,3 +159,7 @@ def ut_pyflann():
     params = flann.build_index(dataset, algorithm='kdtree', trees=4)
 
     result = flann.nn_index(testset, num_neighbors=1)
+
+
+if __name__ == "__main__":
+    ut_estimateCameraRANSAC()
